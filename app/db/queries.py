adb5928064feb3a1d71535eeec59ebe705eec39e
MalsPy <woxa.github.123@gmail.com>
2025-07-30 16:24:10 +0500

    full connection postgresql

--- app/db/queries.py
+++ app/db/queries.py
@@ -1,5 +1,13 @@
+from sqlalchemy import False_, select
+from uuid import UUID, uuid4
+
+from sqlalchemy.engine import result
+from sqlalchemy.orm import session
 from app.db.models import Product, Base
 from app.db.database import async_engine, async_session_factory
+from app.db.models import Product
+from app.fake_db import delete_product_by_id
+from app.schemas import ProductCreate
 
 
 async def create_tables():
@@ -10,3 +18,71 @@ async def create_tables():
 async def drop_tables():
     async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
+
+
+class Products:
+    @staticmethod
+    async def get_all_products():
+        async with async_session_factory() as session:
+            query = select(Product)
+            result = await session.execute(query)
+            products = result.scalars().all()
+            return products
+
+    @staticmethod
+    async def create_product(product_data: ProductCreate):
+        async with async_session_factory() as session:
+            product = Product(id=uuid4(), **product_data.model_dump())
+            session.add(product)
+            await session.commit()
+            await session.refresh(product)
+            return product
+
+    @staticmethod
+    async def get_product_by_id(product_id: UUID):
+        async with async_session_factory() as session:
+            stmt = select(Product).where(Product.id == product_id)
+            result = await session.execute(stmt)
+            product = result.scalar_one_or_none()
+            return product
+
+    @staticmethod
+    async def update_product_fully(product_id: UUID, new_data: ProductCreate):
+        async with async_session_factory() as session:
+            stmt = select(Product).where(Product.id == product_id)
+            result = await session.execute(stmt)
+            product = result.scalar_one_or_none()
+            if not product:
+                return None
+            for key, value in new_data.model_dump().items():
+                setattr(product, key, value)
+            await session.commit()
+            await session.refresh(product)
+            return product
+
+    @staticmethod
+    async def update_product_partially(product_id: UUID, update_data: dict):
+        async with async_session_factory() as session:
+            stmt = select(Product).where(Product.id == product_id)
+            result = await session.execute(stmt)
+            product = result.scalar_one_or_none()
+            if not product:
+                return None
+            for key, value in update_data.items():
+                if hasattr(product, key):
+                    setattr(product, key, value)
+            await session.commit()
+            await session.refresh(product)
+            return product
+
+    @staticmethod
+    async def delete_product_by_id(product_id: UUID) -> bool:
+        async with async_session_factory() as session:
+            stmt = select(Product).where(Product.id == product_id)
+            result = await session.execute(stmt)
+            product = result.scalar_one_or_none()
+            if not product:
+                return False
+            await session.delete(product)
+            await session.commit()
+            return True
--- app/router.py
+++ app/router.py
@@ -3,6 +3,11 @@ from typing import Annotated, List
 from uuid import UUID
 from uuid import uuid4
 
+from sqlalchemy import delete
+from starlette.status import HTTP_404_NOT_FOUND
+from starlette.types import HTTPExceptionHandler
+
+from app.db.models import Product
 from app.schemas import ProductSchemas, ProductCreate
 from app.fake_db import (
     get_fake_db,
@@ -12,54 +17,104 @@ from app.fake_db import (
     update_product_partially,
     delete_product_by_id,
 )
+from app.db.queries import Products
 
 router = APIRouter(prefix="/products", tags=["Products 📦"])
 
+# --- Postgres db ---
 
-@router.get("/get_all", response_model=List[ProductSchemas])
-async def get_product_all(
-    db: Annotated[List[ProductSchemas], Depends(get_fake_db)],
-) -> List[ProductSchemas]:
-    return db
+
+@router.get("/new_get_all", response_model=List[ProductSchemas])
+async def get_all_products():
+    products = await Products.get_all_products()
+    return products
 
 
 @router.get("/get_id/{id}", response_model=ProductSchemas)
-async def get_product_id(id: UUID):
-    product = await get_fake_db_id(id)
+async def get_product_by_id(id: UUID):
+    product = await Products.get_product_by_id(id)
     if not product:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Product not found"
-        )
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Product not found")
     return product
 
 
 @router.post("/post", response_model=ProductSchemas)
 async def post_product(add_product: ProductCreate):
-
-    product = ProductSchemas(id=uuid4(), **add_product.model_dump())
-    await add_new_product(product)
+    product = await Products.create_product(add_product)
     return product
 
 
-@router.patch("/patch/{id}", response_model=ProductSchemas)
+@router.patch("/pathch/{id}", response_model=ProductSchemas)
 async def patch_product(id: UUID, updated_fields: dict):
-    product = await update_product_partially(id, updated_fields)
+    product = await Products.update_product_partially(id, updated_fields)
     if not product:
-        raise HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Product not found")
     return product
 
 
 @router.put("/put/{id}", response_model=ProductSchemas)
 async def put_product(id: UUID, product_data: ProductCreate):
-    updated_product = await update_product_fully(id, product_data)
-    if not updated_product:
-        raise HTTPException(status_code=404, detail="Product not found")
-    return updated_product
+    update_product = await Products.update_product_fully(id, product_data)
+    if not update_product:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Products not fount")
+    return update_product
 
 
 @router.delete("/delete/{id}")
 async def delete_product(id: UUID):
-    deleted = await delete_product_by_id(id)
+    deleted = await Products.delete_product_by_id(id)
     if not deleted:
-        raise HTTPException(status_code=404, detail="Product not found")
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="Product not found")
     return {"success": True, "message": f"Product {id} deleted"}
+
+
+# ---Fake db ---
+
+
+# @router.get("/get_all", response_model=List[ProductSchemas])
+# async def get_product_all(
+#     db: Annotated[List[ProductSchemas], Depends(get_fake_db)],
+# ) -> List[ProductSchemas]:
+#     return db
+#
+#
+# @router.get("/get_id/{id}", response_model=ProductSchemas)
+# async def get_product_id(id: UUID):
+#     product = await get_fake_db_id(id)
+#     if not product:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="Product not found"
+#         )
+#     return product
+#
+#
+# @router.post("/post", response_model=ProductSchemas)
+# async def post_product(add_product: ProductCreate):
+#
+#     product = ProductSchemas(id=uuid4(), **add_product.model_dump())
+#     await add_new_product(product)
+#     return product
+#
+#
+# @router.patch("/patch/{id}", response_model=ProductSchemas)
+# async def patch_product(id: UUID, updated_fields: dict):
+#     product = await update_product_partially(id, updated_fields)
+#     if not product:
+#         raise HTTPException(status_code=404, detail="Product not found")
+#     return product
+#
+#
+# @router.put("/put/{id}", response_model=ProductSchemas)
+# async def put_product(id: UUID, product_data: ProductCreate):
+#     updated_product = await update_product_fully(id, product_data)
+#     if not updated_product:
+#         raise HTTPException(status_code=404, detail="Product not found")
+#     return updated_product
+#
+#
+# @router.delete("/delete/{id}")
+# async def delete_product(id: UUID):
+#     deleted = await delete_product_by_id(id)
+#     if not deleted:
+#         raise HTTPException(status_code=404, detail="Product not found")
+#     return {"success": True, "message": f"Product {id} deleted"}
